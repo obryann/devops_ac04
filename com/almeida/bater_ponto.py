c2e19af7baa8536475b959761a833d885d1ecf6e
Bryan de Almeida <49082707+obryann@users.noreply.github.com>
2019-09-19 20:54:57 -0300

    Criação do método de bater ponto

---
+++ com/almeida/bater_ponto.py
@@ -0,0 +1,11 @@
+from datetime import time
+
+class BaterPonto():
+  entrada = []
+  saida = []
+  
+  def marcar_entrada(self):
+    self.entrada.append(datetime.date)
+    
+  def marcar_saida(self):
+    self.saida.append(datetime.date)
